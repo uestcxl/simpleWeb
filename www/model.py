bc31c31e662e7052985a22d09806c2ce4c76f4c5
xl <zjlsxulei@gmail.com>
2015-08-09 23:51:26 +0800

    添加User，Blog的model类

---
+++ www/model.py
@@ -0,0 +1,37 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Xu Lei'
+
+import time, uuid
+from orm import Model, StringField, IntegerField, BooleanField, TextField, FloatField
+
+
+#返回唯一的id
+def next_int():
+    return '%015d%s' % (int(time.time( * 1000)), uuid.uuid4().hex)
+
+class User(Model):
+    __table__ = 'user'
+
+    id = StringField(primary_key=True, default=next_int, ddl='varchar(50)')
+    email = StringField(ddl='varchar(50)')
+    passwd = StringField(ddl='varchar(50)')
+    admin = BooleanField()
+    name = StringField(ddl='varchar(50)')
+    image = StringField(ddl='varchar(100)')
+    create_at = FloatField(default=time.time)
+
+class Blog(Model):
+    __table__ = 'blogs'
+
+    id = StringField(primary_key=True, default=next_int, ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(50)')
+    name = StringField(ddl='varchar(50)')
+    summary = StringField(ddl='varchar(50)')
+    content = TextField()
+    create_at = FloatField(time.time)
+
+
